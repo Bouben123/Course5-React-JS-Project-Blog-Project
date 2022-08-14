0600b4e31ab4976b5627488e496ed15304bee97b
Bouben123 <jeemin998@gmail.com>
2022-08-14 18:10:41 +0200

    apload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter , Routes , Route } from "react-router-dom";
 import './App.css';
+import Footer from './components/pages/Footer/Footer';
+import Home from './components/pages/HomePage/Home';
+import Navbar from './components/Navbar';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/services' exact element={<Services />} />
+        <Route path='/products' exact element={<Products />} />
+        <Route path='/sign-up' exact element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+);
 }
 
 export default App;
